9b2e7e00c9a51c5f8c739a9892ff51df3be40eb6
Juan Daniel Díaz Matamoros <johnd91@gmail.com>
2024-07-20 17:37:54 +0200

    fix(api): update config

--- src/configuration.h
+++ src/configuration.h
@@ -5,10 +5,10 @@
 
 // Wireless SSID and Password.
 #define WIFI_SSID               "DIGIFIBRA-7GUA"
-#define WIFI_PASSWD             "sKQEeFNFbu24"
+#define WIFI_PASSWD             "xxxxxxxxxxxxxx"
 
 // API REST configuration.
-#define API_HOST                "https://www.postb.in/1721487050677-5813683958258"
+#define API_HOST                "https://www.postb.in/1721489816868-3559819755610"
 #define API_KEY                 "4cae8c84-dd29-42f3-8d58-ed371f1bc8ef"
 
 #define DEVICE_ID               "01J384NT10XHVGYDRXJSXRVAPK"
